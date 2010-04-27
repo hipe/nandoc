b6a4d95cb18d04a0e5f3e8b245dff61475ebe8ed
Mark Meves <mark.meves@gmail.com>
2010-04-27 08:29:08 -0400

    * add: glyphs to site-map generator

--- lib/nandoc/helpers/site-map.rb
+++ lib/nandoc/helpers/site-map.rb
@@ -9,7 +9,10 @@ module NanDoc::Helpers::NanDocHelpers
   #
   def nandoc_sitemap mixed, opts={}, &block
     if mixed.kind_of? Array
-      NanDocSiteMap.new(mixed, opts, &block).render_parent
+      sm = NanDocSiteMap.new(mixed, opts, &block)
+      sm.is_first = true
+      sm.is_last = true
+      sm.render_parent
     elsif mixed.kind_of?(Nanoc3::Item)
       nandoc_sitemap_from_item mixed, opts
     else
@@ -55,6 +58,7 @@ module NanDoc::Helpers::NanDocHelpers
       it.render_child do |m, node| <<-HTML
         <li class='lvl#{m.level}'>
           #{link_to_unless_current(node.nandoc_title, node.path)}
+          #{m.glyphs_right}
           #{m.render_parent}
         </li>
         HTML
@@ -62,8 +66,9 @@ module NanDoc::Helpers::NanDocHelpers
     end
   end
 
-
   class NanDocSiteMap
+    module Glyphs; end # forward declaration
+    include Glyphs
     #
     # @api private
     # this used internally to implement the site maps; you shouldn't
@@ -75,17 +80,22 @@ module NanDoc::Helpers::NanDocHelpers
 
     def initialize children, opts, &block
       @children = children
-      @tab = opts[:tab] || '  '
-      @tabs = opts[:tabs] || 0
-      @level = opts[:level] || 0
+      opts = opts.dup
+      @tab = opts.delete(:tab) || '  '
+      @tabs = opts.delete(:tabs) || 0
+      @level = opts.delete(:level) || 0
+      @opts = opts
       @children_are = @render_child = @render_parent = nil
       @block = block
       yield(self) if block_given?
     end
     attr_reader :level, :tabs, :tab
+    attr_accessor :is_first, :is_last, :parent, :node
+
     def children_are &block;
       @children_are = block
     end
+
     def render_child &block
       raise ArgumentError.new('no') unless block_given?
       @render_child = block
@@ -122,18 +132,29 @@ module NanDoc::Helpers::NanDocHelpers
 
     #
     # you need to return a block of <li>'s lined up appropritately
+    # Go in two passes (you don't have to), first past set things up
     #
     def render_children child_opts
-      cxo = {:level=>@level+1}.merge(child_opts)
+      cxo = @opts.merge(:level=>@level+1).merge(child_opts)
       fail('no') unless cxo[:tab] && cxo[:tabs]
-      these = @children.map do |ch|
+      last_idx = @children.size - 1
+      ch_objs = @children.each.with_index.map do |ch, idx|
         cx = @children_are.call(ch)
-        with_child = self.class.new(cx, cxo, &@block)
-        h1 = @render_child.call(with_child, ch)
-        h2 = with_child.reindent(h1)
+        ch_obj = self.class.new(cx, cxo, &@block)
+        ch_obj.parent = self
+        ch_obj.node = ch
+        ch_obj.is_first = idx == 0
+        ch_obj.is_last  = idx == last_idx
+        ch_obj
+      end
+
+      these = ch_objs.map do |ch_obj|
+        h1 = @render_child.call(ch_obj, ch_obj.node)
+        h2 = ch_obj.reindent(h1)
         h3 = my_strip(h2)
         h3
       end
+
       hz = these.join("\n" + cxo[:tab] * cxo[:tabs])
       hz
     end
@@ -155,6 +176,43 @@ module NanDoc::Helpers::NanDocHelpers
     #
     def my_strip str
       str.strip
+    end
+  end
+
+  module NanDocSiteMap::Glyphs
+    # http://www.alanwood.net/unicode/box_drawing.html
+    Blank  = '&nbsp;'
+    DownLf = '&#9488;' # ┐
+    DownRt = '&#9484;' # ┌
+    HvVtLf = '&#9515;' # ┫
+    HvVert = '&#9475;' # ┃
+    UpLeft = '&#9496;' # ┘
+    UpRite = '&#9492;' # └
+    Vertic = '&#9474;' # │
+    VertLf = '&#9508;' # ┤
+    VertRt = '&#9500;' # ├
+    VtHori = '&#9532;' # ┼
+    ArcUpL = '&#9583;' # ╯
+    DashQV =' &#9482;' # ┊
+
+    UseThisForBlanks = proc{|node| DashQV }
+    # DashQV is nice b/c fixed width
+
+    def glyphs_right_for_child
+      @glyphs_right_for_child ||= begin
+        level == 0 ? [] : begin
+          x = [ is_last ? UseThisForBlanks.call(self) : HvVert ]
+          x.concat(parent.glyphs_right_for_child) if parent
+          x
+        end
+      end
+    end
+
+    def glyphs_right
+      these = [ is_last ? ArcUpL : HvVtLf ]
+      these.concat( parent.glyphs_right_for_child ) if parent
+      # these.concat Array.new([0, level-1].max, DashQV)
+      these.join('')
     end
   end
 end
