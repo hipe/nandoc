f556f85e4f05b90855105c595a69ca0ce0d49ede
Mark Meves <mark.meves@gmail.com>
2010-04-27 08:13:41 -0400

    * add: WHOLE FILES to prototypes.  temporary?

--- proto/default/treebis-task.rb
+++ proto/default/treebis-task.rb
@@ -4,6 +4,9 @@
 # to the folder containing this file.
 
 Treebis.tasks.task(:default) do
+  opts[:patch_hack] = true
+  remove  './content/index.html' # assume we will use README.md for now.
+                                 # we don't want multiple '/' @items.
   apply   './Rules.diff'
   apply   './config.yaml.diff'
   apply   './content/stylesheet.css.diff'
@@ -11,4 +14,5 @@ Treebis.tasks.task(:default) do
   move    './content/stylesheet.css', 'content/css/nanoc-dist-altered.css'
   copy    './content/css/trollop-subset.css'
   apply   './layouts/default.html.diff'
+  apply   './lib/default.rb.diff'
 end
