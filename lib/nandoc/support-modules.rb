481c7ef2c9c4c569c5b4c2d0f18958be28be7471
Mark Meves <mark.meves@gmail.com>
2010-04-30 07:44:53 -0400

    * smell: support modules gets heavy with optparse crap   * smell: optparse crap is duplicating stuff in trollop

--- lib/nandoc/support-modules.rb
+++ lib/nandoc/support-modules.rb
@@ -16,14 +16,82 @@ module NanDoc
       opts.keys.select{|x| x.to_s.index('-') }.each do |k|
         opts[k.to_s.gsub('-','_').to_sym] = opts.delete(k)
       end
+      nil
     end
-    nil
+    def unnormalize_opt_keys keys
+      keys.map{|x| unnormalize_opt_key(x)}
+    end
+    def unnormalize_opt_key key
+      "--#{key.to_s.gsub('_','-')}"
+    end
+
+    #
+    # only call this if you are like a ::Cri::Command object with
+    # all the nanDoc hacks. ick.  This is a temprary hack.  Trollop et al
+    # do this better.
+    #
+    def exclusive_opt_flags opts, &block
+      Exclusive.new(&block).parse(self, opts)
+    end
+
+    class Exclusive
+      include OptsNormalizer
+      def initialize &block
+        @exclusive_flag_keys = nil
+        @default_short = nil
+        @default_key = nil
+        @notice_stream = $stderr
+        instance_eval(&block)
+        fail('definition block needs at least flags()') unless
+          @exclusive_flag_keys
+      end
+      def flags * exclusive_flag_keys
+        @exclusive_flag_keys = exclusive_flag_keys
+      end
+      # params: [short_name_string] name_key
+      def default *a
+        if a.first.kind_of?(String)
+          @default_short = a.shift
+        end
+        if a.first.kind_of?(Symbol)
+          @default_key = a.shift
+        else
+          fail("bad args: #{a.first.inspect}")
+        end
+        fail("extra args: #{a.inspect}") if a.any?
+      end
+      def notice_stream mixed
+        @notice_stream = mixed
+      end
+      def parse cmd, opts
+        these = @exclusive_flag_keys & opts.keys
+        if these.empty? && @default_key
+          if @notice_stream
+            msg =
+            ["using default: "+unnormalize_opt_key(@default_key),
+              @default_short ? "(#{@default_short})" : nil
+            ].compact.join(' ')
+            @notice_stream.puts msg
+          end
+          these.push(@default_key)
+        end
+        if these.size > 1
+          flags = unnormalize_opt_keys(@exclusive_flag_keys)
+          cmd.task_abort <<-ABORT.gsub(/^  */,'')
+            #{flags.join(' and ')} are mutually exclusive.
+            usage: #{cmd.usage}
+            #{cmd.invite_to_more_command_help}
+          ABORT
+        end
+        these.first
+      end
+    end
   end
   module PathHelper
     def assert_path name, *paths
       paths.each do |p|
         unless File.exist?(p)
-          task_abort("#{name} does not exist: #{path}")
+          task_abort("#{name} does not exist: #{p}")
         end
       end
     end
