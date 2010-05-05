435f6585123a4325b74943c704bb0990381ab5ab
Mark Meves <mark.meves@gmail.com>
2010-05-05 11:24:57 -0400

    * add: new filter: command line

--- lib/nandoc/filters.rb
+++ lib/nandoc/filters.rb
@@ -2,6 +2,8 @@ require File.dirname(__FILE__)+'/support/regexp-enhance.rb'
 
 module NanDoc::Filters
   class General < ::Nanoc3::Filter
+    include Nanoc3::Helpers::HTMLEscape
+    alias_method :h, :html_escape
 
     # Parses document contents for nanDoc enhancements to
     # markdown-like markup.
@@ -91,7 +93,7 @@ module NanDoc::Filters
       process_unparsed(unparsed_start, -1)
       @chunks.join('')
     end
-    RecognizedTags = %w(ruby)
+    RecognizedTags = ['ruby', 'from the command line']
     ReFind = /^([[:space:]]*)[ a-z]+:[[:space:]]*\n\1~~~[[:space:]]*\n/m
     ReCapture = %r<
       ^((?:[ ]|\t)*)((?:[a-z]|[ ])+):(?:[ ]|\t)*\n
@@ -116,13 +118,37 @@ module NanDoc::Filters
       end
       case md[:tag_name]
       when 'ruby'; process_block_ruby md
+      when 'from the command line'; process_block_terminal md
       end
     end
+
+    #
+    # hackishly use a subset of the ruby styles to hilight parts
+    # of otherwise black & white console output.
+    # This is not for somehow colorizing colorized output, we will have SpecDoc
+    # for that.
+    #
+    def process_block_terminal md
+      content = reindent_content md
+      lines = ['<pre class="terminal">'] # hack
+      these = content.split("\n").map do |line|
+        line.sub( %r!\A(~[^>]*>)?(.*)\Z! ) do |xx|
+          tags = [];
+          tags.push "<span class='prompt'>#{h($1)}</span>" if $1
+          tags.push "<span class='normal'>#{h($2)}</span>" unless $2.empty?
+          tags.join('')
+        end
+      end
+      lines.concat these
+      lines.push '</pre>'
+      html = lines * "\n" + "\n"
+      @chunks.push html
+    end
     def process_block_ruby md
-      content = reindent_content(md)
+      content = reindent_content md
       hilited = converter_ruby.convert(content, false)
       pre_block = "\n<pre class='ruby'>\n#{hilited}\n</pre>\n"
-      @chunks.push(pre_block)
+      @chunks.push pre_block
     end
     def process_unparsed start, last
       chunk = @content[start..last]
