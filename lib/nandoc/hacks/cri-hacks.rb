f058604bba43c24ed0f644ce82d1a46f7464e698
Mark Meves <mark.meves@gmail.com>
2010-05-17 07:54:58 -0400

    * (cont'd): per rename

--- lib/nandoc/hacks/cri-hacks.rb
+++ lib/nandoc/hacks/cri-hacks.rb
@@ -1,12 +1,13 @@
+require 'nandoc/cli'
+
 module Cri
   class Base
-    # hack
-    include NanDoc::TaskCommon
+    include NanDoc::Cli::CommandMethods
     def remove_command command_class
       if idx = @commands.index{|x| x.kind_of?(command_class) }
         @commands.delete_at(idx)
       else
-        task_abort("command not found of class #{command_class}")
+        command_abort("command not found of class #{command_class}")
       end
     end
   end
