f1cdf5f732cd10e507f0d32b7e08298b018bef70
Mark Meves <mark.meves@gmail.com>
2010-05-17 08:06:42 -0400

    * ÜBERHALL END: broke up the big banks (whew!)

--- lib/nandoc.rb
+++ lib/nandoc.rb
@@ -3,46 +3,23 @@
 
 unless Object.const_defined?('NanDoc')
 
-require 'nanoc3'
-require 'nanoc3/cli'
-
-module NanDoc
+  module NanDoc; end
   #
   # i make the D big so i can see it
   # i move my head away from the microphone when i breathe
   #
-  Root = File.expand_path('../..',__FILE__)
-end
 
-me = File.dirname(__FILE__)+'/nandoc'
 
-# order is important:
-require me + '/support-modules.rb'
-require 'treebis' # gem
-require me + '/config.rb'
+  require 'nanoc3'
+  require 'nanoc3/cli'
+  require 'treebis'
+  me = File.dirname(__FILE__)+'/nandoc'
+  require me + '/core/config.rb'
+  require me + '/hacks.rb'
+  require me + '/commands.rb'
+  # only above should be necessary for 'create site'
 
-module NanDoc
-  Treebis::PersistentDotfile.extend_to(self,
-    './nandoc.persistent.json',
-    :file_utils => Config.file_utils
-  )
-end
-
-# order is not important: (alphabetical:)
-require me + '/commands/create-nandoc-site.rb'
-require me + '/commands/diff.rb'
-require me + '/cri-hacks.rb'
-require me + '/data-source.rb'
-require me + '/filters.rb'
-require me + '/helpers.rb'
-require me + '/item-class-hacks.rb'
-
-Nanoc3::DataSource.register ::NanDoc::DataSource, :nandoc
-Nanoc3::Filter.register ::NanDoc::Filters::General, :nandoc
-
-shared_base = Nanoc3::CLI::Base.shared_base
-shared_base.remove_command Nanoc3::CLI::Commands::CreateSite
-shared_base.add_command    NanDoc::CreateNanDocSite.new
-shared_base.add_command    NanDoc::Commands::Diff.new
-
+  require me + '/helpers.rb'
+  require me + '/filters.rb'
+  require me + '/core/project.rb'
 end
