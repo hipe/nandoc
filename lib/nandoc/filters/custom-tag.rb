6b57c7d5c45c9b243cee054681cfcfb062cf3d81
Mark Meves <mark.meves@gmail.com>
2010-05-17 06:26:20 -0400

    * (cont'd): one day useful for future custom tag API

---
+++ lib/nandoc/filters/custom-tag.rb
@@ -0,0 +1,43 @@
+require 'strscan'
+require File.dirname(__FILE__)+'/tag-parser.rb'
+require 'nandoc/spec-doc'
+
+module NanDoc::Filters
+  class CustomTag
+    include TagParsingInstanceMethods
+    def initialize
+    end
+    def name
+      @name || 'anonymous custom filter'
+    end
+    def tag_parser
+      nil
+    end
+    private :tag_parser
+    def wants? md
+      tp = tag_parser
+      ret = nil
+      if ! tp
+        ret = false
+        @last_failure_lines = ["#{name} doesn't want #{md[:content].inspect}"]
+      else
+        if tree = tp.parse(md[:content])
+          ret = true
+          @last_tree = tree
+        else
+          ret = false
+          these = tp.failure_lines(md[:content])
+          these.unshift("#{name} couldn't parse tag:")
+          @last_failure_lines = these
+        end
+      end
+      ret
+    end
+    def get_failure_lines md
+      @last_failure_lines
+    end
+    def render_block filter, md
+      fail("this is the one you want to implement. look at @last_tree maybe.")
+    end
+  end
+end
