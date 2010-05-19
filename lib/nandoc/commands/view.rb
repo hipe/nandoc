863642195cf902d5baedcb0d3f7b5f59238bc46c
Mark Meves <mark.meves@gmail.com>
2010-05-19 00:53:42 -0400

    * add: new view command that just loads patch and runs old view command

---
+++ lib/nandoc/commands/view.rb
@@ -0,0 +1,15 @@
+module NanDoc::Commands
+  class View < ::Nanoc3::CLI::Commands::View
+    def short_desc
+      spr = super
+      "#{NanDoc::Config.option_prefix}#{spr} (plus blah blah)"
+    end    
+    
+    def run opts, args
+      require 'nandoc/patch'
+      patch = NanDoc::Patch.load(NanDoc::Config.patch_path+'/view-xml-indexes')
+      patch.apply_all
+      super
+    end
+  end
+end
