91395118e13426be22799fad34eaf5c213249881
Mark Meves <mark.meves@gmail.com>
2010-05-19 00:57:19 -0400

    * add: patch command to commands list

--- lib/nandoc/commands.rb
+++ lib/nandoc/commands.rb
@@ -2,8 +2,17 @@ me = File.dirname(__FILE__)
 require 'nandoc/cli'
 require me + '/commands/create-nandoc-site.rb'
 require me + '/commands/diff.rb'
+require me + '/commands/patch.rb'
+require me + '/commands/view.rb'
 
-shared_base = Nanoc3::CLI::Base.shared_base
-shared_base.remove_command Nanoc3::CLI::Commands::CreateSite
-shared_base.add_command    NanDoc::CreateNanDocSite.new
-shared_base.add_command    NanDoc::Commands::Diff.new
+base = Nanoc3::CLI::Base.shared_base
+
+base.remove_command Nanoc3::CLI::Commands::CreateSite
+base.add_command    NanDoc::CreateNanDocSite.new
+
+base.add_command    NanDoc::Commands::Diff.new
+
+base.add_command    NanDoc::Commands::Patch.new
+
+base.remove_command Nanoc3::CLI::Commands::View
+base.add_command    NanDoc::Commands::View.new
