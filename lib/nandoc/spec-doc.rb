aa29638d3c066ca577a412b0a8650dd609e52ed1
Mark Meves <mark.meves@gmail.com>
2010-05-21 17:10:22 -0400

    * change: spec doc to work with moved agents

--- lib/nandoc/spec-doc.rb
+++ lib/nandoc/spec-doc.rb
@@ -1,89 +1,57 @@
-# little hack to a) let clients just include this file for test runs
-# and b) allow us to do the symlink hack for developing this.
 unless Object.const_defined?('NanDoc')
+  # little hack to a) let clients just include this file for test runs
+  # and b) allow us to do the symlink hack for developing this.
+  #
+
   require File.expand_path('../../nandoc.rb', __FILE__)
 end
-
 
 # @todo get this right
 unless NanDoc.const_defined?('SpecDoc') # TO_HERE_HACK
 
+module NanDoc::SpecDoc
+end
+
 me = File.dirname(__FILE__)+'/spec-doc'
-require me + '/agent-instance-methods.rb'
-require me + '/code-snippet.rb'
-require me + '/parse-trace.rb'
-require me + '/recordings.rb'
-require me + '/test-case-agent.rb'
+require me + '/support.rb'
+require me + '/record.rb'
+require me + '/playback.rb'
+require me + '/test-framework.rb'
 
-module NanDoc
-  module SpecDoc
-    class << self
-
-      #
-      # enhance a test framework spec or test case module
-      # for e.g. a minitest spec class.
-      #
-      def include_to mod
-        if Object.const_defined?('MiniTest') &&
-            mod.ancestors.include?(::MiniTest::Spec)
-          require 'nandoc/extlib/minitest.rb'
-          SpecInstanceMethods.include_to mod
-        else
-          GenericInstanceMethods.include_to mod
-        end
-      end
-
-      #
-      # no reason not to use the conventional callback
-      # in cases where we are not passing parameters to the enhancement
-      #
-      alias_method :included, :include_to
-    end
+module NanDoc::SpecDoc
+  class << self
+    include NanDoc::StringMethods # fileize
 
     #
-    # Experimental nandoc hook to give random ass objects a hook to some
-    # kind of SpecDoc agent thing that can a) not necessarily be a test case
-    # but b) still write recordings somehow.  let's see what happens.  hook.
+    # enhance a test framework spec or test case module
+    # for e.g. a minitest spec class.
     #
-    module GenericInstanceMethods
-      class << self
-        def include_to mod
-          mod.send(:include, self)
-        end
-      end
-      def nandoc
-        @nandoc_agent ||= begin
-          require File.dirname(__FILE__)+'/spec-doc/generic-agent'
-          GenericAgent.new(self)
-        end
+    def include_to mod
+      if Object.const_defined?('MiniTest') &&
+          mod.ancestors.include?(::MiniTest::Spec)
+        require 'nandoc/extlib/minitest.rb'
+        SpecMethods.include_to mod
+      else
+        SpecMethodsGeneric.include_to mod
       end
     end
 
-    #
-    # These are the methods that will be available to nandoc-enhanced tests.
-    # For now it is recommended to leave this as just the one method nandoc(),
-    # which will return the TestCaseAgent.
-    #
-    module SpecInstanceMethods
-      class << self
-        def include_to mod
-          #
-          # This used to be MiniTest::Spec-specific, now it's not, but here
-          # for reference:
-          #
-          # unless mod.ancestors.include?(::MiniTest::Spec)
-          #   fail(
-          #    "Sorry, for now SpecDoc can only extend MiniTest::Spec "<<
-          #    " tests.  Couldn't extend #{mod}."
-          #   )
-          # end
-          mod.send(:include, self)
-        end
+    def new_test_framework_proxy name
+      if ! TestFramework.const_defined?(name)
+        dirname = fileize(name)
+        path = "nandoc/spec-doc/test-framework/#{dirname}"
+        require path
       end
-      def nandoc
-        @nandoc_agent ||= TestCaseAgent.new(self)
-      end
+      proxy_class = TestFramework.const_get(name).const_get('Proxy')
+      thing = proxy_class.new
+      thing
     end
+
+    #
+    # no reason not to use the conventional callback
+    # in cases where we are not passing parameters to the enhancement
+    #
+    alias_method :included, :include_to
   end
 end
 
