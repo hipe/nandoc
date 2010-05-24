78ae1b3205f74dd2070a3cffbd94f6ba6b19ffe2
Mark Meves <mark.meves@gmail.com>
2010-05-24 01:13:48 -0400

    * add: two more entities to this very short list

--- lib/nandoc/filters/no-entities.rb
+++ lib/nandoc/filters/no-entities.rb
@@ -5,7 +5,9 @@ module NanDoc::Filters
     # selected subset from:
     # http://en.wikipedia.org/wiki/List_of_XML_and_HTML_character_entity_references
     Map = {
+      'gt'    => 62,
       'ldquo' => 8220,
+      'lt'    => 60,
       'rdquo' => 8221,
       'rsquo' => 8217
     }
